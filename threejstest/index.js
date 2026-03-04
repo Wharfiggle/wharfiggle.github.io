3f4d35cad96c65942ef8cae60043fc7560516907
Elijah Southman <elijahsouthman@gmail.com>
2026-03-04 15:11:40 -0600

    frog

--- threejstest/index.js
+++ threejstest/index.js
@@ -1,5 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "jsm/loaders/GLTFLoader.js";
+
 
 let w = window.innerWidth;
 let h = window.innerHeight;
@@ -16,11 +18,18 @@ const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 camera.position.z = 2;
 const scene = new THREE.Scene();
 
+//gltf loader
+const modelLoader = new GLTFLoader();
+modelLoader.load(
+    "./gremlin_frog/scene.gltf",
+    function(gltf) { scene.add(gltf.scene) }
+)
+
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.03;
 
-const geo = new THREE.IcosahedronGeometry(1.0, 2);
+/*const geo = new THREE.IcosahedronGeometry(1.0, 2);
 const mat = new THREE.MeshStandardMaterial({ color: 0xffffff, flatShading: true });
 const mesh = new THREE.Mesh(geo, mat);
 scene.add(mesh);
@@ -28,9 +37,9 @@ scene.add(mesh);
 const wireMat = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true });
 const wireMesh = new THREE.Mesh(geo, wireMat);
 wireMesh.scale.setScalar(1.001);
-mesh.add(wireMesh);
+mesh.add(wireMesh);*/
 
-const hemiLight = new THREE.HemisphereLight(0x0099ff, 0xaa5500);
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0x000000);
 scene.add(hemiLight);
 
 
@@ -38,7 +47,7 @@ function animate()
 {
     requestAnimationFrame(animate);
     
-    //window resized
+    //adapt to resized window
     if(window.innerWidth != w || window.innerHeight != h)
     {
         w = window.innerWidth;
