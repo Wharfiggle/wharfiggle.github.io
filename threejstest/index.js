e5a3cff6aa95d07fd511917113e40f6db3e2ed3e
Elijah Southman <elijahsouthman@gmail.com>
2026-03-11 01:02:14 -0500

    Simplified + better comments

--- threejstest/index.js
+++ threejstest/index.js
@@ -19,8 +19,37 @@ const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 camera.position.z = 2;
 const scene = new THREE.Scene();
 
+//store materials for later manipulation
+
+//create standard material and inject shader code so that
+//vertex displacement shader is compatible with lighting
+const wobbleMaterial = new THREE.MeshStandardMaterial({ color: "white" });
+wobbleMaterial.onBeforeCompile = (shader) =>
+{
+    shader.uniforms.uTime = { value: 0 };
+
+    shader.vertexShader = `uniform float uTime;
+        ` + shader.vertexShader;
+    //begin_vertex is where transformed is calculated from vertex position
+    shader.vertexShader = shader.vertexShader.replace(
+        "#include <begin_vertex>", `#include <begin_vertex>
+        transformed.y += sin(transformed.x * 10.0 + uTime) * 0.1;
+        `);
+
+    wobbleMaterial.userData.shader = shader;
+    wireMaterial.vertexShader = shader.vertexShader;
+};
+
+//wireframe mesh material
+const wireMaterial = new THREE.ShaderMaterial({
+    wireframe: true,
+    transparent: true,
+    uniforms: { uTime:{value:0}, uOpacity:{value:1.0} },
+    fragmentShader: `uniform float uOpacity;
+        void main() { gl_FragColor = vec4(vec3(1.0, 1.0, 1.0), uOpacity); }`
+});
+
 //gltf loader
-let frogTexture = null;
 const modelLoader = new GLTFLoader();
 modelLoader.load(
     "./gremlin_frog/scene.gltf",
@@ -29,59 +58,20 @@ modelLoader.load(
         gltf.scene.traverse( (child) => {
             if(child.isMesh)
             {
+                //create frog wireframe mesh using wireMaterial
                 const wireMesh = new THREE.Mesh(child.geometry, wireMaterial);
                 wireMesh.scale.setScalar(1.001);
                 scene.add(wireMesh);
                 
-                frogTexture = child.material.map;
+                //apply frog testure to wobbleMaterial
+                wobbleMaterial.map = child.material.map;
+                //apply wobbleMaterial to frog
                 child.material = wobbleMaterial;
             }
         });
         scene.add(gltf.scene)
     }
 )
-
-//store materials for later manipulation
-
-//create standard material and inject shader code so that
-//shader is compatible with lighting
-const wobbleMaterial = new THREE.MeshStandardMaterial({ color: "white" });
-wobbleMaterial.onBeforeCompile = (shader) =>
-{
-    shader.uniforms.uTime = { value: 0 };
-    shader.uniforms.uTexture = { value: frogTexture };
-
-    shader.vertexShader = `varying vec2 vUv;
-        uniform float uTime;
-        ` + shader.vertexShader;
-    //begin_vertex is where transformed is calculated from vertex position
-    shader.vertexShader = shader.vertexShader.replace(
-        "#include <begin_vertex>", `#include <begin_vertex>
-        transformed.y += sin(transformed.x * 10.0 + uTime) * 0.1;
-        vUv = uv;
-        `);
-
-    shader.fragmentShader = `uniform sampler2D uTexture;
-        varying vec2 vUv;
-        ` + shader.fragmentShader;
-    //dont need map_fragment because we're replacing with custom texture
-    shader.fragmentShader = shader.fragmentShader.replace(
-        "#include <map_fragment>",
-        `vec4 texColor = texture2D(uTexture, vUv);
-        diffuseColor *= texColor;
-        `);
-
-    wobbleMaterial.userData.shader = shader;
-    wireMaterial.vertexShader = shader.vertexShader;
-};
-
-const wireMaterial = new THREE.ShaderMaterial({
-    wireframe: true,
-    transparent: true,
-    uniforms: { uTime:{value:0}, uOpacity:{value:1.0} },
-    fragmentShader: `uniform float uOpacity;
-        void main() { gl_FragColor = vec4(vec3(1.0, 1.0, 1.0), uOpacity); }`
-});
 
 //orbit controls
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -91,9 +81,11 @@ controls.dampingFactor = 0.03;
 //lighting
 const hemiLight = new THREE.HemisphereLight("red", "blue");
 scene.add(hemiLight);
+
 const dirLight = new THREE.DirectionalLight("white", 2);
 dirLight.position.set(-5, 5, 5);
 scene.add(dirLight);
+
 const pointLight = new THREE.PointLight("green", 2, 50);
 pointLight.position.set(0.75, -0.25, 0);
 scene.add(pointLight);
