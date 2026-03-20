0b4b1864bf11ed5787ba8cdd98b746b2ff2a2e6a
Elijah Southman <elijahsouthman@gmail.com>
2026-03-20 00:07:48 -0500

    Start to silhouette occlusion shader

--- threejstest/index.js
+++ threejstest/index.js
@@ -16,10 +16,32 @@ const aspect = w / h;
 const near = 0.1;
 const far = 10;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.z = 2;
+camera.position.z = 4;
 const scene = new THREE.Scene();
 
 //store materials for later manipulation
+
+//wireframe mesh material
+const wireMaterial = new THREE.ShaderMaterial({
+    wireframe: true,
+    transparent: true,
+    uniforms: { uTime:{value:0}, uOpacity:{value:1.0} },
+    fragmentShader: `uniform float uOpacity;
+        void main() { gl_FragColor = vec4(vec3(1.0, 1.0, 1.0), uOpacity); }`
+});
+
+//silhouette material
+const silMaterial = new THREE.ShaderMaterial({
+    color: "white",
+    depthTest: true,
+    depthWrite: false,
+    depthFunc: THREE.GreaterDepth,
+    transparent: true,
+    colorWrite: true,
+    uniforms: { uTime:{value:0} },
+    fragmentShader: `
+        void main() { gl_FragColor = vec4(vec3(1.0, 1.0, 1.0), 1.0); }`
+})
 
 //create standard material and inject shader code so that
 //vertex displacement shader is compatible with lighting
@@ -36,18 +58,12 @@ wobbleMaterial.onBeforeCompile = (shader) =>
         transformed.y += sin(transformed.x * 10.0 + uTime) * 0.1;
         `);
 
+    //store for later access
     wobbleMaterial.userData.shader = shader;
+    //apply vertex displacement to wireframe material
     wireMaterial.vertexShader = shader.vertexShader;
+    silMaterial.vertexShader = shader.vertexShader;
 };
-
-//wireframe mesh material
-const wireMaterial = new THREE.ShaderMaterial({
-    wireframe: true,
-    transparent: true,
-    uniforms: { uTime:{value:0}, uOpacity:{value:1.0} },
-    fragmentShader: `uniform float uOpacity;
-        void main() { gl_FragColor = vec4(vec3(1.0, 1.0, 1.0), uOpacity); }`
-});
 
 //gltf loader
 const modelLoader = new GLTFLoader();
@@ -62,8 +78,12 @@ modelLoader.load(
                 const wireMesh = new THREE.Mesh(child.geometry, wireMaterial);
                 wireMesh.scale.setScalar(1.001);
                 scene.add(wireMesh);
+
+                //add silhouette mesh using silMaterial
+                const silMesh = new THREE.Mesh(child.geometry, silMaterial);
+                scene.add(silMesh);
                 
-                //apply frog testure to wobbleMaterial
+                //apply frog texture to wobbleMaterial
                 wobbleMaterial.map = child.material.map;
                 //apply wobbleMaterial to frog
                 child.material = wobbleMaterial;
@@ -91,6 +111,14 @@ pointLight.position.set(0.75, -0.25, 0);
 scene.add(pointLight);
 
 
+const obst = new THREE.Mesh(
+    new THREE.IcosahedronGeometry(1.0, 2),
+    new THREE.MeshStandardMaterial({ color: "purple", flatShading: true })
+)
+scene.add(obst);
+obst.position.z = 2;
+
+
 //tick
 function animate(t = 0)
 {
@@ -99,6 +127,7 @@ function animate(t = 0)
     //pulse wireMaterial opacity
     wireMaterial.uniforms.uOpacity.value = Math.sin(t * 0.001) / 2;
     wireMaterial.uniforms.uTime.value = t * 0.001;
+    silMaterial.uniforms.uTime.value = t * 0.001;
     if(wobbleMaterial.userData.shader)
         wobbleMaterial.userData.shader.uniforms.uTime.value = t * 0.001;
 
